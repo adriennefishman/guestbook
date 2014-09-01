f92a3762a310a83aca1afa42da2762144df7c303
github username <github email address>
2014-09-01 01:18:15 -0400

    initial commit

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'guestbook.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #hello
+    #hi
 
     url(r'^admin/', include(admin.site.urls)),
 )
